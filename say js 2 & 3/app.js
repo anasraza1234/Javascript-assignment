c7f7fbd4332ad1579af132b382981de2551eca6e
Anas Raza <154012203+anasraza1234@users.noreply.github.com>
2024-03-17 17:19:31 +0500

    Add files via upload

---
+++ say js 2 & 3/app.js
@@ -0,0 +1,63 @@
+
+// assignment 02
+
+
+// alert("hello world")
+
+
+// let myName = "anas raza"
+// alert(myName)
+
+// let age = 17 
+// alert(age)
+
+// let courceName = "Web & mobile app development"
+// alert(courceName)
+
+
+// let food = "pizza \n pizz \n piz \n pi \n p"
+// alert(food)
+
+
+
+// let email = "my email address is anasraza23@gmail.com"
+// alert(email)
+
+
+
+// let book = "i am trying to lern from the book A smarter \n way to learn JavaScript"
+// alert(book)
+
+
+
+// let doc = "yah! i can write HTML content through javascript"
+// document.write(doc)
+
+
+
+// let design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(design)
+
+
+
+
+// assignment 03
+
+
+
+
+// let age = "i am 18 year old"
+// alert(age)
+
+
+// let birthYear = "my birth year is 2005"
+// document.write(birthYear );
+
+
+
+// var store1 = prompt("enter your name")
+// var store2 = prompt("products")
+// var store3 = +prompt("enter a quantity")
+// var store4 = "on XYZ clothing store"
+
+// document.write(store1,"  orderd ", store2 ,   store3,  store4)
