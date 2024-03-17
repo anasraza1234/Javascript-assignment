75a313c324524594fbd4fe894ce5df5e8ad08992
Anas Raza <154012203+anasraza1234@users.noreply.github.com>
2024-03-17 23:56:21 +0500

    Add files via upload

---
+++ assi 3 chap 4 & 5/app.js
@@ -0,0 +1,47 @@
+
+// assign // chapter 4
+
+// let myBikeName = "Honda", myCarName = "civic", name = "anas raza";
+
+// alert(myBikeName+" "+myCarName+" "+name);
+
+
+// Legal variable names:
+// let myName = "anas raza";
+// let $full_Name = "anas raza";
+// let _myname = "anas raza";
+// let myFirstName = "anas";
+// let age = 18;
+
+
+// Illegal variable names:
+
+// let 5number; 
+// let my variable; 
+// let my-variable; 
+// let my*variable; 
+// let let; 
+
+
+// assignment 03 chapter 05
+
+
+
+// function addNumbers(num1, num2) {
+//     return num1 + num2;
+//   }
+
+
+// let number1 = parseFloat(prompt("Enter the first number"));
+// let number2 = parseFloat(prompt("Enter the second number"));
+
+
+// let sum = addNumbers(number1, number2);
+
+
+// document.write("The sum of " + number1 + " and " + number2 + " is: " + sum);
+
+
+
+
+
