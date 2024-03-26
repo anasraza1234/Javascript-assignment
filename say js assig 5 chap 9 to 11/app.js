fb6d83773dc907dab0752349ac80b4fcfa8a0b5b
Anas Raza <154012203+anasraza1234@users.noreply.github.com>
2024-03-26 23:16:17 +0500

    Add files via upload

---
+++ say js assig 5 chap 9 to 11/app.js
@@ -0,0 +1,105 @@
+
+// let cityname = prompt("enter your city name")
+
+// if(prompt = "karachi"){
+//     document.write("'welcome to city f lights'")
+// }
+
+
+// let gender = prompt("enter your gender")
+
+// if(gender === "male"){
+//     alert("Good morning sir")
+// }
+
+// else{ 
+//     alert("Good morning maam")
+// }
+    
+
+
+// let traffiColor = prompt("enter a traffic color") 
+
+// if(traffiColor === "red"){
+//     alert("must stop")
+// }
+
+// if(traffiColor === "yellow"){
+//     alert("ready to move")
+// }
+
+// if(traffiColor === "green"){
+//     alert("move now")
+// }
+
+// else{
+//     alert("enter invalid color")
+// }
+
+
+
+// var favColor = prompt("Enter Your Favourite Color");
+// if (favColor === "green" || favColor === "blue" || favColor === "red") {
+//     alert("Congratulation")
+// } else {
+//     alert("Sorry")
+// }
+
+
+
+// let fuel = +prompt("enter remaining fuel in car")
+
+// if(fuel < 0.25){
+//     alert("please refill the fuel in your car")
+// }
+
+
+
+
+// var a = 8;
+// if (++a === 10){
+// alert("given condition for variable a is true");
+// }
+
+// var b = 164;
+// if (b++ === 166){
+// alert("given condition for variable b is true");
+// }
+// var c = 24;
+// if (c++ === 26){
+// alert("condition 1 is true");
+// }
+// if (c === 26){
+// alert("condition 2 is true");
+// }
+// if (++c < 28){
+// alert("condition 3 is true");
+// }
+// if(c === 28){
+// alert("condition 4 is true");
+// }
+// var materialCost = 90000;
+// var laborCost = 9000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+//  if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+//  if("car" < "cat"){
+//     alert("car is smaller than cat");
+//  }
+
+// let obtainedmarks = +prompt("enter your obtained mark")
+// let tatalmarks = 500
+// let perc = (obtainedmarks * 100 / tatalmarks)%
+
+// if(perc === 90%) {
+//    alert("A grade")
+// }
+
