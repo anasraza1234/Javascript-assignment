68dc83f7db32502915e4a1e27982bc0e2b76dda0
Anas Raza <154012203+anasraza1234@users.noreply.github.com>
2024-04-21 06:27:18 -0700

    Add files via upload
    javascript chapter 6 to 9

---
+++ say js assi 4 chap 6 to 9/app.js
@@ -0,0 +1,80 @@
+// var a = 10;
+// var preNum = ++a;
+// document.write('The Value Of ++a Is',a);
+// document.write('Now The Value Of A Is',preNum);
+
+// // Prefix Decrement
+// document.write(`Prefix Increment`);
+// var a = 10;
+// var preNum = --a;
+// document.write('Now The Value Of A Is',a);
+// document.write('The Value Of A Is',preNum)  ;
+
+// // Post Increment
+// console.log(`Post Increment`);
+// var a = 10;
+// var preNum = a++;
+// document.write('The Value Of A Is',a)
+// document.write('Now The Value Of A Is',preNum )
+ 
+// // Post Decrement
+// document.write(`Post Decrement`);
+// var a = 10;
+// var preNum = a--;
+// document.write('Now The Value Of A Is',a);
+// document.write('The Value Of A Is',preNum);
+
+
+
+
+
+
+
+
+
+
+
+
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// console.log(result);
+// --a;
+// console.log(a);
+// --a - --b;
+// console.log(a,b);
+// --a - --b + ++b;
+// console.log(a,b,b);
+// --a - --b + ++b +b--;
+// console.log(a,b,b);
+
+
+
+
+
+
+// let userName = prompt(`enter your name`,`anas raza`)
+// document.write(`hello ${userName}`)
+ 
+
+// var five = prompt(`Enter Any Number`,5);
+// document.write(five*5);
+
+
+
+
+
+// var urdu = 75;
+// var social = 85;
+// var Eng = 60;
+
+// var totalMarks = 300;
+// var obtainMarks = 220;
+
+// var percentage = obtainMarks * 100 / totalMarks;
+// document.write(percentage);
+
+// // Get the user's name
+// const user_name = prompt("Enter your name:");
+
+// // Greet the user
+// document.write(`Hello, ${user_name}! Welcome to our program. 😊`);
