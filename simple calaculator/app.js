19d36ad8a265b205e902abdf32c253ed34d2be15
Anas Raza <154012203+anasraza1234@users.noreply.github.com>
2024-04-22 11:50:12 -0700

    Add files via upload
    simple calculator by javascript

---
+++ simple calaculator/app.js
@@ -0,0 +1,10 @@
+
+
+document.write("This is my Calculator <br>")
+var num1 = +prompt("Enter a value");
+var num2 = +prompt("Enter a value");
+document.write("The Addition of two values is", num1 + num2,
+ "<br>The Subtraction of two values is ", num1-num2,
+ "<br>The Mulipication of two values is ", num1*num2,
+  "<br>The Division of two values is ", num1/num2,
+   "<br>The Modulus of two values is ", num1%num2 );
