a48a512deced622fad8c32744091c5454fa9cab0
Anas Raza <154012203+anasraza1234@users.noreply.github.com>
2024-04-22 08:56:16 -0700

    Add files via upload
    js assign 6 chap 12 & 13

---
+++ chapter 12&13/app.js
@@ -0,0 +1,73 @@
+
+// let character = prompt("Enter Character");
+// console.log(character.toLowerCase() , character.toUpperCase());
+
+
+
+
+
+// let num1 = +prompt("enter a first number")
+// let num2 = +prompt("enter a second number")
+
+// if(num1 > num2){
+//     document.write("num 1 is a larger number")
+// }
+
+// else if(num1 < num2){
+//     document.write("num 2 is a larger number")
+// }
+
+// else{
+//     document.write("please enter a valid number")
+// }
+
+
+
+
+
+
+
+// let userNum = +prompt("enter a number")
+
+// if(userNum > 10){
+//     document.write("it is positive number")
+// }
+
+// else if(userNum > 1 ){
+//     document.write("it is negative number")
+// }
+
+// else{
+//     document.write( "it is zero")
+// }
+
+
+
+
+
+
+// let vowels = prompt("Enter a Vowels").toLowerCase();
+
+// if(vowels === "a" || vowels === "e" || vowels === "i" || vowels === "o" || vowels === "u") {
+//     document.write(vowels + " is a vowels");
+// }
+// else{
+//     document.write(vowels + " is not a vowels");
+// }
+
+
+
+
+// let userPass = prompt("Enter a Password");
+// let myPass = 12345;
+
+// if (userPass){
+//     if(userPass==myPass){
+//         console.log("Correct!");
+//     }
+//     else{
+//         console.log("Incorrect password");
+//     }
+// }else{
+//     console.log("Please enter your password");
+// }
