a95976c403933083e780aeb676c88a717b760408
Anas Raza <154012203+anasraza1234@users.noreply.github.com>
2024-04-22 11:40:23 -0700

    Add files via upload
    age calculator by javascript

---
+++ age calculator/app.js
@@ -0,0 +1,11 @@
+function calculator (){
+    alert("age calcuator")
+    let currentYear = 2024
+    let userAge = prompt("enter your age")
+    let age = currentYear - userAge
+    alert(`your birth year is ${age}`)
+}
+
+calculator()
+
+
