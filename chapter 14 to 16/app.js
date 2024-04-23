1649540e9318c5da09148060fd635b1cd75632f4
Anas Raza <154012203+anasraza1234@users.noreply.github.com>
2024-04-23 10:13:13 -0700

    Add files via upload
    arrays chapter 14 to 16

---
+++ chapter 14 to 16/app.js
@@ -0,0 +1,104 @@
+
+// let empty1 = []
+
+
+
+// let empty2 = new Array()
+
+
+
+// let word = ["anas","raza","qadri"]
+
+
+
+// let num = [1,2,3,4,5,6,7,8,9,0]
+
+
+
+// let mixArr = ["anas",18,true]
+
+
+
+// let education = ["e.g" ," ", "SSC"," ", "HSC"," BCS","BCOM", "MS", 'M.Phil'," PhD"]
+// document.write(education)
+
+
+
+
+
+// let stdName = ["anas","ali","yasir"]
+// let score = [400,420,440]
+// let totalmarks = 500
+
+// let percn = [score * 100  /  totalmarks]
+
+// document.write(`student name: ${stdName[i]} `)
+// document.write(`score: ${score[i]}`)
+// document.write(`percentage: ${percn[1]} %`)
+
+
+
+
+
+
+// question no 09
+
+// let colorName = ["red","  ","blue","  " ,"green", "  ","yellow","  ","white","  ",'black']
+// document.write(colorName)
+
+// a
+// let colorName = ["red","  ","blue","  " ,"green", "  ","yellow","  ","white","  ",'black']
+// let usercolor = prompt("enter your color if you want to add in begining")
+// colorName.unshift(usercolor)
+// document.write(colorName)
+
+// .b
+// let colorName = ["red","  ","blue","  " ,"green", "  ","yellow","  ","white","  ",'black']
+// let usercolor = prompt("enter your color if you want to add in end")
+// colorName.push(usercolor)
+// document.write(colorName)
+
+
+// .c
+// let colorName = ["red","  ","blue","  " ,"green", "  ","yellow","  ","white","  ",'black']
+// let usercolor1 = prompt("enter your 1st color if you want to add in end")
+// let usercolor2 = prompt("enter your 2nd color if you want to add in end")
+// colorName.unshift(usercolor1 , usercolor2)
+// document.write(colorName)
+
+// .d
+// let colorName = ["red","  ","blue","  " ,"green", "  ","yellow","  ","white","  ",'black']
+// colorName.shift()
+// document.write(colorName)
+
+
+// .e
+// let colorName = ["red","  ","blue","  " ,"green", "  ","yellow","  ","white","  ",'black']
+// colorName.pop()
+// document.write(colorName)
+
+
+// .f
+// let colorName = ["red","  ","blue","  " ,"green", "  ","yellow","  ","white","  ",'black']
+// let userinput1 = prompt("enter index number")
+// let userinput2 = prompt("enter how many remove")
+// let userinput3 = prompt("enter your color in which index you want to add")
+// colorName.splice(userinput1,userinput2,userinput3)
+// document.write(colorName)
+
+
+// .g
+// let colorName = ["red","  ","blue","  " ,"green", "  ","yellow","  ","white","  ",'black']
+// let userinput1 = prompt("enter index number")
+// let userinput2 = prompt("enter how many remove")
+// colorName.splice(userinput1,userinput2,)
+// document.write(colorName)
+
+
+
+
+// questin no 11
+
+// let citiesName = ["karachi","lahore","peshawar","islamabad","multan","quetta"]
+// docume.write(`Cities list: ${citiesName}`) 
+// document.write(`Selected cities list: ${citiesName.slice(4,6)}`)
