5c437e5ac49c87917ee8eb29a5205cb7e374b2fe
Anas Raza <154012203+anasraza1234@users.noreply.github.com>
2024-05-05 02:15:30 -0700

    Add files via upload

---
+++ assignment 8 chp 17 to 20/app.js
@@ -0,0 +1,115 @@
+
+// 02
+// let metrix = [
+//   [0,1,2,3],
+//   [1,0,1,2],
+//   [2,1,0,1]
+// ]
+
+
+// document.write(metrix);
+
+
+
+
+
+
+// 03
+// let i = 1;
+
+// for (let i = 1; i<=10 ; i++){
+//     document.write(`${i}  </br>`)
+// }
+
+
+
+
+
+
+// 04
+// let userTable = +prompt("enter table number")
+// let userLenth = +prompt("enter table lenth")
+
+// for (let i = 1; i <= userLenth; i++){
+//     document.write(`${userTable} x ${i} = ${userTable * i} </br> `)
+// }
+
+
+
+
+
+
+
+
+
+// 05
+// let fruits = ["apple", "banana", "mango", "orange","strawberry"];
+
+
+// for(let i=0; i<fruits.length; i++){
+//     document.write(fruits[i] + "<br>");
+// }
+
+// for(let i=0; i<fruits.length; i++){
+//     document.write(`Element at index ${i} is ${fruits[i]} <br>`);
+// }
+
+
+
+
+
+
+
+
+
+
+// 06
+// let counting = [1,2,3,4,5,6,7,8,9,];
+
+
+//     document.write(`COUNTING: </br> ${counting} </br>`)
+
+
+// let a = [1,2,3,4,5,6,7,8,9,];
+// let reverse = a.reverse()
+// document.write(`REVERSE COUNTING: </br> ${reverse}  </br> `)
+
+
+
+// let even = [0,2,4,6,8,10,12,14,16,18,20] 
+//     document.write(`EVEN: </br> ${even} </br>`)
+
+//  let odd = [1.3,5,7,9,11,13,15,17,19,21] 
+//     document.write(`ODD: </br> ${odd}`)
+
+
+// let series = [2,3,4,5,6,7,8,9,] 
+// document.write(`SERIES: </br> ${series.join("k,")}`)   
+
+
+
+
+
+
+
+
+// 07
+// let items = ["cake", "apple"," pie", "cookie", "chips", "patties"]
+
+// let userSearch = prompt("search items").toLowerCase();
+// let ispresent = items.includes(userSearch)
+
+// if(ispresent){
+//     document.write(`${userSearch} is available in our bakery`)
+// }
+
+// else{
+//     document.write(`we are sorry ${userSearch}  is not available in our bakery`)
+// }
+
+
+
+
+
+
+
